567b7f79c0e3b485245ac0c4fd489bc6d0759c10
Abenezerzewdu <abenezerzewdu11@gmail.com>
2025-05-25 00:49:52 +0300

    finished

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import Counter from './component/Counter';
 function App() {
   return (
     <div className="App">
-     <h1>Counter</h1>
+      <h1>Counter</h1>
+      <Counter/>
     </div>
   );
 }
--- counter/src/component/Counter.js
+++ counter/src/component/Counter.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import { useState } from 'react'
+import '../component/counter.css'
 function Counter() {
 const [count,setCount]=useState(0);
 const [step,setStep]=useState(1);
 
+function handleAddition(){
+    parseInt(step);
+ setCount(count+step);
+ 
+}
+function handleSubtraction(){
+    if(count===0)return;
+    setCount(count-step);
+
+}
+const handleReset=()=>{
+    setCount(0);
+    setStep(1)
+}
   return (
-    <div>Counter</div>
+    <>
+  <div className='app_container'>
+    <div className='container'>
+    <div id='count'>Count : {count}</div>
+   <span className='step'>
+   Step : <input id='input' value={step} onChange={(e)=>setStep(Number(e.target.value))}></input>
+    </span> 
+    </div>
+    <div>
+        <button onClick={handleAddition}> + </button> 
+        <button onClick={handleSubtraction}>-</button> </div>
+    <div>
+        <button onClick={handleReset}>Reset</button>
+    </div>
+ </div>
+    </>
+     
   )
 }
 
